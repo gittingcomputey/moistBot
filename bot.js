0e765a5185dd05b28ad00df8f28ac42b4362a710
Ubuntu <ubuntu@ip-172-31-44-254.us-east-2.compute.internal>
2020-06-18 01:07:37 +0000

    Clean up unused function and comments.

--- bot.js
+++ bot.js
@@ -67,25 +67,12 @@ bot.on('message', msg => {
 // Embedded message
 bot.on('message', msg => {
   if (msg.content === 'embed') {
-    
-		// Info about justiceScratch message embed  
+		// Info about MaximumMoist server -  message embed
     const infoembed = new MessageEmbed()
       .setTitle('MaximumMoist')
       .setColor('#2e76cf')
       .setDescription('Moist to the maximum');
     	msg.channel.send(infoembed);
-  }
-});
-
-
-//attachment
-bot.on('message', message => {
-	
-  if (message.content === 'michael-annoyed') {
-    // Create the attachment using MessageAttachment
-    const attachment = new MessageAttachment('./images/the_office/michael/michael-annoyed.jpg');
-    // Send the attachment in the message channel
-    message.channel.send(attachment);
   }
 });
 
@@ -292,53 +279,6 @@ bot.on('message', msg => {
                         break;
 		}
 });
-
-
-// Pic Logic
-bot.on('message', message => {
-  // Ignore messages that aren't from a guild
-  if (!message.guild) return;
-
-  if (message.content.startsWith('kick this mofo')) {
-    // Assuming we mention someone in the message, this will return the user
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = message.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
-        member
-          .kick('Optional reason that will display in the audit logs')
-          .then(() => {
-            // We let the message author know we were able to kick the person
-            message.reply(`Successfully kicked ${user.tag}`);
-          })
-          .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to kick the member,
-            // either due to missing permissions or role hierarchy
-            message.reply('I was unable to kick the member');
-            // Log the error
-            console.error(err);
-          });
-      } else {
-        // The mentioned user isn't in this guild
-        message.reply("That user isn't in this guild!");
-      }
-      // Otherwise, if no user was mentioned
-    } else {
-      message.reply("You didn't say who to kick");
-    }
-  }
-});
-
-
 
 //login
 bot.login(configFile.token);
